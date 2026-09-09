227bb3f602d22dab6860dd3b55dc42c6a43f92a3
toto <aaa@ff.fr>
2026-09-09 18:52:28 +0200

    maj

--- TP1/fonction.py
+++ TP1/fonction.py
@@ -1,10 +1,11 @@
-import fonction as f
+def puissance(a, b):
+    return a ** b
+
 
 while True:
     try:
         a = int(input("Entrez la base a : "))
         b = int(input("Entrez l'exposant b : "))
-        res = f.puissance(a, b)
-        print(f"{a}^{b} = {res}")
+        print(f"{a}^{b} = {puissance(a, b)}")
     except ValueError:
         print("Veuillez entrer des entiers valides !")
